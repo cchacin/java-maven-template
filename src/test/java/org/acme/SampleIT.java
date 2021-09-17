ec51e534fd4cf95b97245207c585bdf075dc31d6
Carlos Chacin <cchacin@gmail.com>
2021-09-17 01:23:00 -0700

    feat: Add WithAssumptions interface to the tests

--- src/test/java/org/acme/SampleIT.java
+++ src/test/java/org/acme/SampleIT.java
@@ -1,9 +1,10 @@
 package org.acme;
 
 import org.assertj.core.api.WithAssertions;
+import org.assertj.core.api.WithAssumptions;
 import org.junit.jupiter.api.Test;
 
-class SampleIT implements WithAssertions {
+class SampleIT implements WithAssertions, WithAssumptions {
 
     @Test
     void test() throws Exception {
--- src/test/java/org/acme/SampleTest.java
+++ src/test/java/org/acme/SampleTest.java
@@ -1,9 +1,10 @@
 package org.acme;
 
 import org.assertj.core.api.WithAssertions;
+import org.assertj.core.api.WithAssumptions;
 import org.junit.jupiter.api.Test;
 
-class SampleTest implements WithAssertions {
+class SampleTest implements WithAssertions, WithAssumptions {
 
     @Test
     void test() throws Exception {
