ed2e58dcc38ce179b7637ff37294620073adfcb1
Carlos Chacin <cchacin@gmail.com>
2023-06-05 23:24:48 -0700

    fix: Clean up

--- lib/src/main/java/org/acme/Function.java
+++ lib/src/main/java/org/acme/Function.java
@@ -1,13 +1,13 @@
 package org.acme;
 
-public class Sample {
+public class Function {
 
-    public Output call(
+    record Input(int x, int y) {}
+    record Output(long result) {}
+
+    public Output add(
             final Input input) {
         System.out.println("input = " + input);
         return new Output(input.x() + input.y());
     }
-
-    record Input(int x, int y) {}
-    record Output(long result) {}
 }
--- lib/src/test/java/org/acme/FunctionTest.java
+++ lib/src/test/java/org/acme/FunctionTest.java
@@ -2,16 +2,16 @@ package org.acme;
 
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
-import org.acme.Sample.Input;
+import org.acme.Function.Input;
 import org.assertj.core.api.WithAssertions;
 import org.assertj.core.api.WithAssumptions;
 
-class SampleTest
+class FunctionTest
         implements WithAssertions, WithAssumptions {
 
     @Property
     void sumTwoValuesIsValid(@ForAll final int x, @ForAll final int y) {
-        assertThat(new Sample().call(new Input(x, y)).result())
+        assertThat(new Function().add(new Input(x, y)).result())
                 .isBetween(Long.MIN_VALUE, Long.MAX_VALUE);
     }
 }
