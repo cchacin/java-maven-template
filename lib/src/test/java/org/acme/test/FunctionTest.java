50bee2112509b879970fb0232f9bbe1232bff192
Carlos Chacin <cchacin@gmail.com>
2024-12-30 23:51:20 -0800

    fix: Enabling spotless plugin

--- lib/src/test/java/org/acme/test/FunctionTest.java
+++ lib/src/test/java/org/acme/test/FunctionTest.java
@@ -1,15 +1,13 @@
 package org.acme.test;
 
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
 import org.acme.Function;
 import org.acme.Function.Input;
 import org.assertj.core.api.WithAssertions;
 import org.assertj.core.api.WithAssumptions;
 
-import net.jqwik.api.ForAll;
-import net.jqwik.api.Property;
-
-class FunctionTest
-        implements WithAssertions, WithAssumptions {
+class FunctionTest implements WithAssertions, WithAssumptions {
 
     @Property
     void sumTwoValuesIsValid(@ForAll final int x, @ForAll final int y) {
