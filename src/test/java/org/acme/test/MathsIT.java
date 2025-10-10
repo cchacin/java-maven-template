a1d738b8df34427b1a7a0c306d4adfd3a506202e
Carlos Chacin <cchacin@gmail.com>
2025-10-09 23:43:42 -0700

    fix: @UseType in property tests

--- src/test/java/org/acme/test/MathsIT.java
+++ src/test/java/org/acme/test/MathsIT.java
@@ -2,6 +2,7 @@ package org.acme.test;
 
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
+import net.jqwik.api.constraints.UseType;
 import org.acme.Maths;
 import org.acme.Maths.Input;
 import org.assertj.core.api.WithAssertions;
@@ -10,8 +11,7 @@ import org.assertj.core.api.WithAssumptions;
 class MathsIT implements WithAssertions, WithAssumptions {
 
     @Property
-    void sumTwoValuesIsValid(@ForAll final int x, @ForAll final int y) {
-        assertThat(new Maths().sum(new Input(x, y)).result())
-                .isBetween(Long.MIN_VALUE, Long.MAX_VALUE);
+    void sumTwoValuesIsValid(@ForAll @UseType Input input) {
+        assertThat(new Maths().sum(input).result()).isBetween(Long.MIN_VALUE, Long.MAX_VALUE);
     }
 }
